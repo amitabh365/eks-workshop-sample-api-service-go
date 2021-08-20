d1600c8d53a6e2b54c360146f2692884a8adcfdf
amitabh365 <65166007+amitabh365@users.noreply.github.com>
2021-08-20 17:39:31 +0530

    Commit

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World v0.6"}
+		res := &response{Message: "Hello World v0.7"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
